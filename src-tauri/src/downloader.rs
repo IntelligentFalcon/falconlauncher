29827675866656f60cb77d7a0aeadef7867dad90
Mmd4J <hosun1451@gmail.com>
2025-05-06 22:42:38 +0330

    Working on downloading library

--- src-tauri/src/downloader.rs
+++ src-tauri/src/downloader.rs
@@ -1,5 +1,7 @@
+use crate::structs;
 use crate::structs::AssetIndex;
 use serde_json::Value;
+use std::env::vars_os;
 use tauri::async_runtime::{block_on, spawn};
 
 /// Returns the version_manifest.json file in a Value structure if the parse process was succeed.
@@ -14,8 +16,16 @@ pub fn load_version_manifest() -> Some(Value) {
     json
 }
 
-pub fn load_version(url: String) {
+fn load_version(url: String) {
     // TODO: load version json file.
+    let mut json = None;
+    block_on(async {
+        let result = reqwest::get(url).await.expect("Failed to download file.");
+        let text = result.text().await.expect("Failed to read file.");
+        json = Some(text);
+    });
+
+    spawn(async {});
 }
 
 fn load_version_assets(asset_index: AssetIndex) {
@@ -26,3 +36,43 @@ pub fn download_version(id: String) {
     let manifest = load_version_manifest();
     //TODO: Some tasks like downloading version libraries & assets and stuff should be done here.
 }
+
+fn download_libraries(value: Value) {
+    let libraries = value
+        .get("libraries")
+        .expect("Parsing libraries of version failed!")
+        .as_array()
+        .expect("Transforming libraries data to array failed");
+    for library in libraries {
+        let library_name = library
+            .get("name")
+            .expect("Parsing library_name failed")
+            .as_str()
+            .expect("Parsing library_name failed");
+        let os = get_current_os();
+        let rules = library.get("rules");
+        match rules {
+            Some(rules) => {
+                if rules
+                    .get("os")
+                    .unwrap()
+                    .get("name")
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+                    == os
+                {
+                    //TODO: download library since it requires the same os that user have
+                }
+            }
+            Err(..) => {
+                //TODO: download library since it doesnt have rules to disallow any os
+            }
+        }
+    }
+}
+
+pub fn get_current_os() -> String {
+    structs::parse_os(sys_info::os_type().expect("Unsupported Operating System"))
+}
+fn download_library(library_value: Value) {}
--- src-tauri/src/structs.rs
+++ src-tauri/src/structs.rs
@@ -17,9 +17,27 @@ pub enum VersionType {
 }
 
 pub struct AssetIndex {
-    id: String,
-    sha1: String,
-    size: u64,
-    total_size: u64,
-    url: String,
+    pub id: String,
+    pub sha1: String,
+    pub size: u64,
+    pub total_size: u64,
+    pub url: String,
+}
+
+pub struct LibraryInfo {
+    pub name: String,
+    pub sha1: String,
+    pub size: u64,
+    pub path: String,
+    pub uri: String,
+}
+
+pub enum OperatingSystem {
+    Windows,
+    Linux,
+    MacOS,
+}
+
+pub fn parse_os(os: String) -> String {
+    os.to_lowercase().replace("darwin", "osx")
 }
