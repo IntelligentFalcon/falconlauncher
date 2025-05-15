7ec7d64119bc74095d08507ed2266d538031ab6a
Maploop <sweatypvpmc@gmail.com>
2025-05-16 00:43:07 +0330

    Disable DevTools menu

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+  });
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
