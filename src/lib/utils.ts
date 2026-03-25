8b2e785c60c55da2410b2adb40d2112c7986cda0
Gnkalk <Gnkalk@yahoo.com>
2026-03-25 18:40:50 +0330

    add lots of component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { InvokeError, Invokes } from '@/invokes';
+import { invoke } from '@tauri-apps/api/core';
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function backend<T extends keyof Invokes>(
+  name: T,
+  args?: Invokes[T]['args'],
+): Promise<Invokes[T]['returns']> {
+  return new Promise((resolve, reject) => {
+    invoke<Invokes[T]['returns']>(name, args)
+      .then((result) => {
+        resolve(result);
+      })
+      .catch((error: InvokeError<Invokes[T]['custom_error']>) => {
+        reject(error);
+      });
+  });
 }
