6e2e1ec153f63fa9341b5c0eb84f2d0a9d0aff82
mmd4j <hosun1451@gmail.com>
2026-07-24 22:03:12 +0330

    feat: clean code

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -59,9 +59,9 @@ pub static GLOBAL_CACHE: LazyLock<sync::Mutex<Global>> = LazyLock::new(|| {
     })
 });
 
-pub const DEV_MODE: bool = true;
+pub const DEV_MODE: bool = false;
 pub const LAUNCHER_NAME: &str = "FalconLauncher";
-pub const LAUNCHER_VERSION: &str = "BETA";
+pub const LAUNCHER_VERSION: &str = "BETA-0.1";
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     dotenvy::dotenv().ok();
